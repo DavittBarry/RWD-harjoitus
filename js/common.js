d39d1633806f79e11cfc74a6d6e75a4121438643
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2021-09-13 14:01:54 +0300

    0.0.1 versio
    JavaScriptia laitettu menu-valikkoon. Dynaamisuutta laajennettu.

--- js/common.js
+++ js/common.js
@@ -1,12 +1,31 @@
+var header = document.querySelector('header');
 var menuBtn = document.getElementById('menuBtn');
 var menu = document.getElementById('menu');
-var closeMenuBtn = document.getElementById('closeMenuBtn')
+var closeMenuBtn = document.getElementById('closeMenuBtn');
+var lastScrollTop = 0;
 
 // Menu button click event handler
 menuBtn.onclick = function() {
     menu.classList.add('show');
+    menuBtn.classList.add('hide');
 };
 
 closeMenuBtn.onclick = function() {
     menu.classList.remove('show');
+    menuBtn.classList.remove('hide');
+};
+
+window.onscroll = function () {
+    var currentScrollTop = document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop && currentScrollTop > header.offsetHeight) {
+        console.log('Going down...')
+        header.classList.add('hide')
+        }
+        else {
+            console.log('Going up...')
+            header.classList.remove('hide');
+        
+    }
+    lastScrollTop = currentScrollTop;
 };
