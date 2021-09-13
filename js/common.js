926f021e31ebd1ec72f9fb844333f1296d31ec18
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2021-09-13 14:06:33 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,31 +1,12 @@
-var header = document.querySelector('header');
 var menuBtn = document.getElementById('menuBtn');
 var menu = document.getElementById('menu');
-var closeMenuBtn = document.getElementById('closeMenuBtn');
-var lastScrollTop = 0;
+var closeMenuBtn = document.getElementById('closeMenuBtn')
 
 // Menu button click event handler
 menuBtn.onclick = function() {
     menu.classList.add('show');
-    menuBtn.classList.add('hide');
 };
 
 closeMenuBtn.onclick = function() {
     menu.classList.remove('show');
-    menuBtn.classList.remove('hide');
-};
-
-window.onscroll = function () {
-    var currentScrollTop = document.documentElement.scrollTop;
-
-    if (currentScrollTop > lastScrollTop && currentScrollTop > header.offsetHeight) {
-        console.log('Going down...')
-        header.classList.add('hide')
-        }
-        else {
-            console.log('Going up...')
-            header.classList.remove('hide');
-        
-    }
-    lastScrollTop = currentScrollTop;
 };
