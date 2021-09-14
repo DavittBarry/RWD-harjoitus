08842bd9b3c39f8ba5dfba7f605d82e004ea5b66
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2021-09-14 11:30:26 +0300

    0.0.3
    Vielä kehitystä...

--- js/common.js
+++ js/common.js
@@ -2,7 +2,15 @@ var header = document.querySelector('header');
 var menuBtn = document.getElementById('menuBtn');
 var menu = document.getElementById('menu');
 var closeMenuBtn = document.getElementById('closeMenuBtn');
+var menuLinks = document.querySelectorAll('#menu a');
 var lastScrollTop = 0;
+
+function setHash(hash) {
+    var el = document.querySelector(hash)
+    el.id = hash + '-tmp';
+    window.location.hash = hash;
+    el.id = hash.replace('#', '');
+}
 
 // Menu button click event handler
 menuBtn.onclick = function() {
@@ -21,11 +29,33 @@ window.onscroll = function () {
     if (currentScrollTop > lastScrollTop && currentScrollTop > header.offsetHeight) {
         console.log('Going down...')
         header.classList.add('hide')
-        }
-        else {
-            console.log('Going up...')
+    }
+        else if (currentScrollTop) {
+            header.classList.add('condensed');
             header.classList.remove('hide');
-        
+    }
+
+
+    if (currentScrollTop <= header.offsetHeight) {
+        header.classList.remove('condensed');
     }
     lastScrollTop = currentScrollTop;
-};
+}
+    for (var i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].onclick = function(e) {
+        e.preventDefault();
+        var sel = this.getAttribute('href');
+        var el = document.querySelector(sel);
+        el.scrollIntoView({
+            behavior: 'smooth'
+        });
+        setHash(sel);
+        menu.classList.remove('show');
+        menuBtn.classList.remove('hide');
+        setTimeout(function() {
+            header.classList.add('hide');
+        }, 1000);
+    };
+}
+
+
